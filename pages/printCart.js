a1b70df9ab8100e55eb503197df6811823dc9df3
juancarlossrp-ms <jjcarlos853@gmail.com>
2021-03-24 17:27:25 -0300

    'sss'

--- pages/printCart.js
+++ pages/printCart.js
@@ -20,13 +20,13 @@ const useStyles = makeStyles((theme) => ({
   image: {
     width: 194,
     height: 194,
-	marginTop: "-28%",
-    marginLeft: "-14%"
+	marginTop: "-31%",
+    marginLeft: "-20%"
   },
   img: {
     margin: 'auto',
     display: 'block',
-    maxWidth: '75%',
+    maxWidth: '62%',
     maxHeight: '75%',
   },
  
@@ -43,7 +43,7 @@ function Carteira (){
   const classes = useStyles();
 	const [logo, setLogo] = useState("");
 
-	const storageRef = fire.storage().ref();
+	/*const storageRef = fire.storage().ref();
 	storageRef.child('logo.jpg').getDownloadURL().then(function(url) {
 	  // `url` is the download URL for 'images/stars.jpg'
 
@@ -58,7 +58,7 @@ function Carteira (){
 		setLogo(url);
 	}).catch(function(error) {
 	  // Handle any errors
-	});
+	});*/
 	
 	const ref = React.createRef();
 	
@@ -77,30 +77,31 @@ function	printDocument() {
 	
 return(
    <div>
+
     <div ref={ref} id="divToPrint" className={classes.root}>
 	
       <Paper className={classes.paper} style={{maxHeight:"200px", maxWidth:"335px"}}>
         <Grid container spacing={0} style={{maxHeight:"97px"}}>
           <Grid item>
             <ButtonBase className={classes.image}>
-              <img className={classes.img} alt="complex" src="logo.jpg" />
+              <img className={classes.img} style={{ maxWidth: '62%'}} alt="complex" src="logo.jpg" />
             </ButtonBase>
           </Grid>
-          <Grid item xs={12} sm container style={{marginLeft:"-16%"}}>
+          <Grid item xs={12} sm container style={{marginLeft:"-23%",marginRight:"-4%"}}>
             <Grid item xs container direction="column" spacing={0} style={{marginTop:"-6%"}}>
               <Grid item xs>
                 <Typography align="center" style={{
-					fontWeight: 'bold', fontFamily: '-webkit-body', letterSpacing: '-3px', fontSize: '33px'}}  variant="h5">
+					fontWeight: 'bold', fontFamily: '-webkit-body', letterSpacing: '-1px', fontSize: '33px'}}  variant="h5">
                   S    I    N   D I F P M
                 </Typography>
-                <Typography style={{fontSize: "0.68rem", marginTop:"-2%"}} align="center" variant="h6"  >
+                <Typography style={{fontSize: "0.8rem", marginTop:"-5%"}} align="center" variant="h6"  >
 					<Box lineHeight={1} m={1}>
 					  Sindicato dos Funcionários Públicos Municipais de Bataguassu - MS
 					</Box>
                 </Typography>
 				
-                <Typography align="center" variant="caption" color="" style={{    fontSize: "0.44rem"}}>
-					<Box lineHeight={1} m={1}>
+                <Typography align="center" variant="caption" color="" style={{    fontSize: "0.54rem"}}>
+					<Box lineHeight={1} m={1} style={{marginRight:"2%",marginLeft:"-1%", marginTop:"-2%"}}>
 					 Reg. Min. Trabalho e Emprego 46.000.004428/01-11<br/>
 					 Rua Brasilândia, Nº 495 - Centro <br/>
 					 Fone: (67) 3541-1065
@@ -115,40 +116,40 @@ return(
           </Grid>
         </Grid>
 		<Grid container spacing={1}>
-          <Grid item>
+		<Grid item  style={{padding:"2px"}}>
 			 <Typography align="center" variant="body2" style={{fontSize: "0.68rem"}} color="">
 					Nome: 
               </Typography>
 		  </Grid>
 		</Grid>
 		<Grid container spacing={1}>
-          <Grid item>
+          <Grid item style={{padding:"2px"}}>
 			 <Typography align="center" variant="body2" style={{fontSize: "0.68rem"}} color="">
                  Função: 
               </Typography>
 		  </Grid>
 		</Grid>
 		<Grid container spacing={1}>
-          <Grid item xs={12} sm={6} >
+          <Grid item xs={12} sm={6} style={{padding:"2px"}}>
 			 <Typography  variant="body2" style={{fontSize: "0.68rem"}} color="">
                  Data de Admissão: 
               </Typography>
 		  </Grid>
-		  <Grid item xs={12} sm={6}>
+		  <Grid item xs={12} sm={6} style={{padding:"2px"}}>
 			 <Typography  variant="body2" style={{fontSize: "0.68rem"}} color="">
                  Válida até: 
               </Typography>
 		  </Grid>
 		</Grid>
-		<Grid container spacing={2}>
-          <Grid item xs={12}>
+		<Grid container spacing={2} >
+          <Grid item xs={12} style={{padding:"6px"}}>
 			 <Typography align="right" style={{fontSize: "0.68rem"}} variant="body2" color="">
                  Bataguassu 12 de Outubro de 2020
               </Typography>
 		  </Grid>
 		</Grid>
 		<Grid container spacing={7}>
-          <Grid item xs={12}>
+          <Grid item xs={12} style={{padding:"22px"}}>
 			 <Typography align="center" style={{fontSize: "0.68rem"}} variant="body2" color="">
                  ____________________
 				 

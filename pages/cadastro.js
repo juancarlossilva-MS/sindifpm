0021f5735ca85832eb8910e3ce0bd4d51d6f613d
juancarlossilva-ms <jjcarlos853@gmail.com>
2021-05-21 23:00:50 -0300

    'tod'

--- pages/cadastro.js
+++ pages/cadastro.js
@@ -16,10 +16,6 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
-
-
-
-
 function Cadastro() {
 
     

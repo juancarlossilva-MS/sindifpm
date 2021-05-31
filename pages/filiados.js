7b398d8643bf05267e117899abc6cf93610d4ad6
juancarlossrp-ms <jjcarlos853@gmail.com>
2021-05-31 10:17:56 -0300

    '3105'

--- pages/filiados.js
+++ pages/filiados.js
@@ -654,7 +654,12 @@ function MoldeCarteira(){
 }
 
 function dataNasc(data){
-	return data[8]+data[9]+"/"+data[5]+data[6]+"/"+data[0]+data[1]+data[2]+data[3];
+	
+	if(!data){
+		return "";
+	}else{
+		return data[8]+data[9]+"/"+data[5]+data[6]+"/"+data[0]+data[1]+data[2]+data[3];
+	}
 }
 
 function OpenCheckBox(){ setOpenmul(!openmul); }
